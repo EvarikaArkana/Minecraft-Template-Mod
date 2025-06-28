b1fd90a77d2ae386b8d2676b7e2b1d2573f6f1d8
EvarikaArkana <evarikaarkana@proton.me>
2025-06-28 00:28:04 -0400

    improving template example mixins
    Signed-off-by: EvarikaArkana <evarikaarkana@proton.me>

--- src/main/java/eva/template/mixin/client/ExampleMixin.java
+++ src/main/java/eva/template/mixin/client/ExampleMixin.java
@@ -5,7 +5,7 @@ import com.mojang.authlib.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(MinecraftClient.class)
-public class ExampleTemplateMixin {
+public class ExampleMixin {
 
 
 
